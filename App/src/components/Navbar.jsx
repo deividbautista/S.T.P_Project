4b4e875f5854ace641600c70d3caf78304cdcaa1
camiloogarcia <camilo140515but@gmail.com>
2025-01-21 21:05:58 -0500

    botones

--- App/src/components/Navbar.jsx
+++ App/src/components/Navbar.jsx
@@ -5,11 +5,19 @@ import image from '../assets/img/T&F.png'
 const Navbar = () => {
     return (
         <nav className='Navbar'>
-            <div className='Navbar_logo'>   
+            <div className='Navbar_logo'>
                 <img className='Logo' src={image} alt="" />
             </div>
-            <p>Cristian chupon</p>
-        </nav>                  
+            <div className='navbar-collapse'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'><a href="">Nosotros</a></li>
+                    <li className='nav-item'><a href="">Servicios</a></li>
+                    <li className='nav-item'><a href="">Productos</a></li>
+                    <li className='nav-item'><a href="">PQr</a></li>
+                    <li className='nav-item'><a href="">Contactenos</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
