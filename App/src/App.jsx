e957923854a0e8b3a7033bd0aae43081897a9f13
deividbautista <debautistaocampo@gmail.com>
2025-01-22 19:40:03 -0500

    Sincronización de ramas

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,20 +1,21 @@
 import './App.css'
-import Navbar from './components/NavBar'
 
 function App() {
 
   return (
     <>
-    <header>
-      <Navbar Texto = "hola" >
-      </Navbar>
-    </header>
-      <div>
-      <p className="read-the-docs">
-        Hola mundo
-      </p>
-      </div>
-    
+      <body>
+        <header>
+          <title>malparido</title>
+        </header>
+        <div>
+          <p className="read-the-docs">
+            Hola mundo
+          </p>
+        </div>
+
+      </body>
+
     </>
   )
 }
