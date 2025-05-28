0163a106dc2f196b82c588b81513a01c51052009
deividbautista <debautistaocampo@gmail.com>
2025-05-28 09:25:54 -0500

    configuration tittles

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,17 +1,36 @@
+import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home'
 
-function App() {
+function usePageTitle() {
+  const location = useLocation ();
 
+  useEffect (() => {
+    const path = location.pathname;
+
+    const titles = {
+      '/': 'Inicio',
+      '/Products': 'Productos',
+    };
+  
+  document.title = titles[path] || 'Mi proyecto';
+ }, [location]);
+}
+
+function AppRoutes() {
+  usePageTitle();
+  
   return (
-    <>
-    <header>
-      <Home/>
-    </header>
-      <div>
-      </div>
-     
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
   )
 }
 
-export default App
+export default function App(){
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+}
